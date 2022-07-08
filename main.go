810e7991010d2a8f151311bcb1c2f9fcd8b92b49
Maor Friedman <mafriedm@redhat.com>
2022-07-08 15:03:20 +0300

    get prometheus rules (todo fix namespace)

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
+	ctrl "sigs.k8s.io/controller-runtime"
+)
+
+func GetPrometheusRules() ([]monitoringv1.PrometheusRule, error) {
+	ctx := context.Background()
+	config := ctrl.GetConfigOrDie()
+	dynamic := dynamic.NewForConfigOrDie(config)
+
+	resourceId := schema.GroupVersionResource{
+		Group:    "monitoring.coreos.com",
+		Version:  "v1",
+		Resource: "prometheusrules",
+	}
+	list, err := dynamic.Resource(resourceId).Namespace("observatorium-stage").
+		List(ctx, metav1.ListOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	prometheusRules := make([]monitoringv1.PrometheusRule, len(list.Items))
+	for idx, item := range list.Items {
+		obj := monitoringv1.PrometheusRule{}
+		j, _ := json.Marshal(item.Object)
+		json.Unmarshal(j, &obj)
+		prometheusRules[idx] = obj
+	}
+
+	return prometheusRules, nil
+}
 
 func main() {
-	fmt.Println("hello world!")
+	prometheusRules, _ := GetPrometheusRules()
+	for _, pr := range prometheusRules {
+		fmt.Println(pr.Name)
+	}
 }
